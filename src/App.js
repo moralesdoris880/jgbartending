6201a55854f340dfb6bbfef8a615c9ddded17f4c
Doris Morales <moralesdoris880@gmail.com>
2024-01-18 13:26:21 -0500

    two header elements added with css

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
       <h3>J&G</h3>
       <h1>Mobile Bar</h1>
       <h2>Bartending Services</h2>
+      <h4>
+        <span>Book</span>
+        <span>your</span>
+        <span>Event</span>
+        <span>Today</span>
+      </h4>
       <footer>
         <p>Designed by Doris Morales</p>
       </footer>
