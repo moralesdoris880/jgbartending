af17d6486fbd101f81d4fd2e7cd3365230921af8
Doris Morales <moralesdoris880@gmail.com>
2024-01-19 14:13:54 -0500

    added footer elements, fixed logo font

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import './App.css';
+import React from 'react';
 import Nav from './Nav.js';
 
 function App() {
+
+  const drink = React.useRef(null)
+
+  const handleScroll = (ref) => {
+    window.scrollTo({
+      top: ref.offsetTop,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="App" alt="Image by kjpargeter on Freepik">
       <Nav />
+      <div id="cocktail"></div>
       <h3>J&G</h3>
       <h1>Mobile Bar</h1>
       <h2>Bartending Services</h2>
-      <h4>
-        <span>Book</span>
+      <h4><a href="mailto:andradejc82@gmail.com">
+        <span>&#62;&#62; Book</span>
         <span>your</span>
         <span>Event</span>
-        <span>Today!</span>
+        <span>Today! &#60;&#60;</span>
+      </a>
       </h4>
       <div id="borderEventList">
-        <h5>Specialized In:</h5>
+        <h5>Our Services</h5>
         <div id="eventList">
           <div className="listColumn">
             <li>Wedding Reception</li>
@@ -32,10 +46,18 @@ function App() {
         </div>
         <p id="andMoreText">And More!!!</p>
       </div>
-      
-      <footer>
-        <p>Designed by Doris Morales</p>
+      <div><span style={{left:'48%',fontSize:'48px'}} className="material-symbols-outlined" onClick={() => {handleScroll(drink.current)}}>expand_more</span></div>
+      <footer ref={drink}>
+        <h6>J&G</h6>
+        <div id="breakline"></div>
+        <div id="footerinfo">
+          <h7>Get in Touch</h7>
+          <div id="call" style={{}}><span className="material-symbols-outlined" style={{fontSize:'24px',marginLeft:'10px',width:'25px',height:'25px'}}>call</span><span className="number" href="tel:+19294189777"><a className='phonenumber'>929-418-9777</a></span></div>
+          <div id="call2" style={{}}><span className="material-symbols-outlined" style={{fontSize:'24px',marginLeft:'10px',width:'25px',height:'25px'}}>call</span><span className="number"><a href="tel:+16465093905" className='phonenumber'>646-509-3905</a></span></div>
+          <div id="call3" style={{}}><span class="material-symbols-outlined" style={{fontSize:'24px',marginLeft:'10px',width:'25px',height:'25px'}}>mail</span><span className="number">andradejc82@gmail.com</span></div>
+        </div>
       </footer>
+      <p id="designcreds">Copyright@2024 Designed by Doris Morales</p>
     </div>
   );
 }
